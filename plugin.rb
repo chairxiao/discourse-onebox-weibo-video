73e551099241bc924422b4402c9842c98a3acd45
chairxiao <1277163268@qq.com>
2020-01-12 22:39:10 +0800

    edit html part

--- plugin.rb
+++ plugin.rb
@@ -2,37 +2,34 @@
 
 # name: discourse-onebox-weibo-video
 # about: For generating pretty oneboxes for video.weibo.com
-# version: 0.1
+# https://video.h5.weibo.cn/1034:4459699299352603/4459699502527906
+# version: 0.2
 # authors: dengzi
 
-Onebox = Onebox
+# Onebox = Onebox
 
 module Onebox
-  module Engine
-     class WeiboVideoOnebox
-        include Engine
-        include StandardEmbed
-
-        always_https
-        matches_regexp(/^(https?:\/\/)?video\.h5\.weibo\.(com|cn)\/(.)+\/?$/)
-
-        WIDTH  ||= 480
-        HEIGHT ||= 360
-
-        def to_html
-
-           <<~HTML
-           <iframe src="#{@url}"
-           width="#{WIDTH}"
-           height="#{HEIGHT}"
-           frameborder='0'
-           allowfullscreen >
-           </iframe>
-
-           HTML
-        end
-     end
-  end
-end
-
-
+   module Engine
+      class WeiboVideoOnebox
+         include Engine
+ 
+         always_https
+         matches_regexp(/^(https?:\/\/)?video\.(h5\.)?weibo\.(com|cn)\/(.)+\/?$/)
+ 
+         WIDTH  = 640
+         HEIGHT = 420
+ 
+         def to_html
+          "<iframe src='#{@url}'
+            width='#{WIDTH}'
+            height='#{HEIGHT}'
+            scrolling='no'
+            frameborder='0'
+            webkitallowfullscreen
+            mozallowfullscreen
+            allowfullscreen >
+            </iframe>" 
+         end
+      end
+   end
+ end
