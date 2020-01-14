e188f8436519bba1a11e1391c7b71908f5a38947
dengcd <1277163268@qq.com>
2020-01-14 09:55:46 +0800

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -3,12 +3,12 @@
 # name: discourse-onebox-weibo-video
 # about: For generating pretty oneboxes for video.weibo.com
 # https://video.h5.weibo.cn/1034:4459699299352603/4459699502527906
-# version: 0.2
+# version: 0.3
 # authors: dengzi
 
 Onebox = Onebox
 
-register_asset "javascripts/weibo.js"
+# register_asset "javascripts/weibo.js"
 
 module Onebox
   module Engine
@@ -25,12 +25,12 @@ module Onebox
 
          "
          <iframe 
-           id='weiboiframe'
+           name='wbframe'
            src='#{@url}'
            width='#{WIDTH}'
            height='#{HEIGHT}'
            scrolling='no'
-           frameborder='0'
+           frameborder='1'
            webkitallowfullscreen
            mozallowfullscreen
            allowfullscreen >
